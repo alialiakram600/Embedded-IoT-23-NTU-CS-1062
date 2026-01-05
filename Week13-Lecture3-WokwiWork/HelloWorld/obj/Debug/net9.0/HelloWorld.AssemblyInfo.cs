4d1ba6eacc0e05cf2a172cc0583d1c95043383d5
Ali Akram <alialiakram600@gmail.com>
2026-01-05 10:57:04 +0500

    fin

--- Week13-Lecture3-WokwiWork/HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ Week13-Lecture3-WokwiWork/HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08af934d8e492e579b0cb7a34499819532748010")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2024771fce18d87e4bddfdd725b5c921f2c4d09c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
